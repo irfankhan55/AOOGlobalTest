10a582d98276bea8771309d263b5d7c8e162beb8
Muhammad Irfan Khan <irfank480@gmail.com>
2022-06-24 15:50:51 +0800

    added unit test for dropDown

--- src/_tests_/ui-tests/DropDown.test.js
+++ src/_tests_/ui-tests/DropDown.test.js
@@ -6,9 +6,10 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 
 describe('DropDown Testing', () => {
     const renderer = new ShallowRenderer();
-    const placeholder = 'Select your country'
+    const placeholder = 'Select your Langauge'
+
     const mockCountries = [{ name: 'English (UK)', value: 'en' },
-    { name: 'English (UK)', value: 'en' },
+    { name: 'English (US)', value: 'en' },
     { name: 'French ', value: 'fr' },
     { name: 'Danish ', value: 'da' }]
   
@@ -16,14 +17,22 @@ describe('DropDown Testing', () => {
         const component = renderer.render(<Dropdown onSelect={() => { }} placeholder={placeholder} defaultSelected={mockCountries[0]} data={mockCountries} />);
         expect(component).toMatchSnapshot()
     });
+
     it('DropDown should have correct placeholder', () => {
-        const testRenderer = TestRenderer.create(<Dropdown onSelect={() => { }} placeholder='Select your country' defaultSelected={mockCountries[0]} data={mockCountries} />);
+        const testRenderer = TestRenderer.create(<Dropdown onSelect={() => { }} placeholder={placeholder} defaultSelected={mockCountries[0]} data={mockCountries} />);
         const testInstance = testRenderer.root;
         expect(testInstance.props.placeholder).toBe(placeholder);
     });
+
     it('DropDown should have correct data', () => {
-        const testRenderer = TestRenderer.create(<Dropdown onSelect={() => { }} placeholder='Select your country' defaultSelected={mockCountries[0]} data={mockCountries} />);
+        const testRenderer = TestRenderer.create(<Dropdown onSelect={() => { }} placeholder={placeholder} defaultSelected={mockCountries[0]} data={mockCountries} />);
         const testInstance = testRenderer.root;
         expect(testInstance.props.data.length).toBe(4);
+    });
+
+    it('DropDown should have correct default selected', () => {
+        const testRenderer = TestRenderer.create(<Dropdown onSelect={() => { }} placeholder={placeholder} defaultSelected={mockCountries[0]} data={mockCountries} />);
+        const testInstance = testRenderer.root;
+        expect(testInstance.props.defaultSelected).toBe(mockCountries[0]);
     });
 })          
